90cfe2fdc6a62e2238c77ca3f50cc873269d0f78
Sorasful <teva.krief@hotmail.fr>
2020-10-18 19:59:01 +0200

    Add __init__.py

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
 from nordvpn_connect import initialize_vpn, rotate_VPN, close_vpn_connection
 
-destination = "France"
+destination = "italy"
 
 nordvpn_username = ""
 nordvpn_password = ""
 
-settings = initialize_vpn(destination, nordvpn_username, nordvpn_password)
+settings = initialize_vpn(destination)
 rotate_VPN(settings)
 
 # YOUR STUFF
---
+++ nordvpn_connect/__init__.py
@@ -0,0 +1,1 @@
+from .nordvpn_connect import *
